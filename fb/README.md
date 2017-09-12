a3afcb368b8239f525583aeaaadcb47def9e8b5e
Dhaval Mehta <dhaval.mehta@eliteinfoworld.com>
2017-09-12 12:14:40 +0530

    add firebase

---
+++ fb/README.md
@@ -0,0 +1,2 @@
+# firebase-data-post-and-get
+"POST" and "GET" javascript method for a form and firebase database
---
+++ fb/script.js
@@ -0,0 +1,9 @@
+//// Get a database reference to our posts
+//var ref = new Firebase("https://docs-examples.firebaseio.com/web/saving-data/fireblog/posts");
+//
+//// Attach an asynchronous callback to read the data at our posts reference
+//ref.on("value", function(snapshot) {
+//  console.log(snapshot.val());
+//}, function (errorObject) {
+//  console.log("The read failed: " + errorObject.code);
+//});
