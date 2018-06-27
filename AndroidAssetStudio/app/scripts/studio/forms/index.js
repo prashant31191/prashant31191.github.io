cb46922e749021ce6bba155e5a5861944832e239
prashant patel <prashant.chovatiya@eliteinfoworld.com>
2018-06-27 12:31:30 +0530

    Update Studio UI

---
+++ AndroidAssetStudio/app/scripts/studio/forms/index.js
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2016 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {BooleanField} from './BooleanField';
+import {ColorField} from './ColorField';
+import {EnumField} from './EnumField';
+import {Field} from './Field';
+import {Form} from './Form';
+import {ImageField} from './ImageField';
+import {RangeField} from './RangeField';
+import {TextField} from './TextField';
+
+export const forms = {
+  BooleanField,
+  ColorField,
+  EnumField,
+  Field,
+  Form,
+  ImageField,
+  RangeField,
+  TextField,
+};
